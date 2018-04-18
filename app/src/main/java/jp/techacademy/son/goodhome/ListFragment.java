117fc4184f6e87cfdee82d8c887bc42cb73779f3
sonyanai <taison0930@outlook.jp>
2018-04-18 14:54:20 +0900

    bottomNavigationMenu完成 オプションメニュー アカウントの作成 ログイン customerログイン プリファレンスに保存できてるかの確認 checkboxの取得と保存と初期値の設定 ログアウト 変数名修正 customerData businessaccount,login customeraccountをpreference使わずに表示 login終わり
    editTextのせいげん
    電話番号とかメールアドレスとか
    customerlogin,businessloginで必須が入力されているか
    その他を選んだときのみ入力できるようにする
    アカウント作成時，ログイン時，loginfragmentの読み込み時に読み込んでますよダイアログの表示
    
    いろんな業種があるからログイン時に何の業者かラジオボタンで選択させる
    評価のkey欲しい業種も
    businessログイン作成から

--- app/src/main/java/jp/techacademy/son/goodhome/ListFragment.java
+++ app/src/main/java/jp/techacademy/son/goodhome/ListFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
